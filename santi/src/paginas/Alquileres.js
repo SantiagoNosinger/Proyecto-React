7ac51ae294829e1c797ae914436cfe10b3a0fe62
SantiagoNosinger <snosingermail.com>
2023-10-17 11:09:35 -0300

    hice cambios

--- santi/src/paginas/Alquileres.js
+++ santi/src/paginas/Alquileres.js
@@ -11,7 +11,7 @@ export default class Alquileres extends React.Component{
                     <div class="row p-4 text-center">
                         <div class="col-12 col-sm-6 mx-auto">               
                             <h1 class="texto">Pablo M.<span class="segunda-linea texto"> Nösinger</span></h1>
-                            <h5 class="pt-5 text-white">Profesional experto y apasionado que brinda atención personalizada del más alto nivel en la búsqueda de tu hogar. Nuestra experiencia permitirá que tu compra o venta sea un éxito.</h5>
+                            <h5 class="pt-5 text-white">Profesional experto y apasionado que brinda atención personalizada del más alto nivel en la búsqueda de tu hogar. Nuestra experiencia permitirá que tu compra y venta sea un éxito.</h5>
                         </div>
                     </div>
                 </div>
